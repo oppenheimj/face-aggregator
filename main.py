26f48a0e2acf8c22cb538f34aa2daba68741b286
Jonas Oppenheim <oppenheim10@gmail.com>
2020-11-23 01:50:29 -0800

    Detect best smiles for every faceSet

--- main.py
+++ main.py
@@ -12,3 +12,11 @@ ag = Aggregate(batch, batch.images[0], batch.images[1].getFaces())
 # put onto a.jpg, the girl's face from b.jpg
 ag.swapAllFaces("test.jpg")
 # batch.draw()
+
+
+batch.identifyHappiestFaces()
+# At this point, every faceSet in the batch knows the best face
+# and from what image it originated. All that's left to do is
+# swap!
+
+# batch.draw()
