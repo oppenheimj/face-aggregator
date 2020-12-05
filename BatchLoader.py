bca1e749831b7e546c9de83ac9baafc1fa0469d3
Jérémie Allard <jerall24@gmail.com>
2020-12-05 10:39:21 -0500

    Modified call to implement (image, path) tuple in arguments

--- BatchLoader.py
+++ BatchLoader.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog
 import cv2
+from log import log
 
 from Batch import Batch
 
@@ -13,6 +14,7 @@ class BatchLoader(object):
         root.withdraw()
 
         filePaths = filedialog.askopenfilenames()
-        images = [cv2.imread(path) for path in filePaths]
+        images = [(cv2.imread(path), path) for path in filePaths]
+        # log.info(f"Images and paths being sent to Batch {images}")
 
         return Batch(images)
