11893371187484f77761ca5d6d89294516504f62
Jonas Oppenheim <oppenheim10@gmail.com>
2020-12-08 03:27:52 -0800

    Allow 'draw' to be passed as command line arg

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from log import log
 from BatchLoader import BatchLoader
 from Aggregate import Aggregate
 
+import sys
+
+args = sys.argv
+
 batchLoader = BatchLoader()
 batch = batchLoader.browse()
 
@@ -9,8 +13,11 @@ batch.detectFaces()
 batch.generateFaceSets()
 batch.identifyHappiestFaces()
 
-# Create Aggregate class to add the best faces onto the chosen image
-ag = Aggregate(batch, batch.getUserScene(), batch.getHappiestFaces())
+if 'draw' in args:
+    batch.draw()
+else:
+    # Create Aggregate class to add the best faces onto the chosen image
+    ag = Aggregate(batch, batch.getUserScene(), batch.getHappiestFaces())
 
-output_path = input("Type desired file name (no extension) or press Enter to use default file name: ")
-ag.swapAllFaces(f'{output_path if len(output_path) else "best_image"}.jpg')
+    output_path = input("Type desired file name (no extension) or press Enter to use default file name: ")
+    ag.swapAllFaces(f'{output_path if len(output_path) else "best_image"}.jpg')
